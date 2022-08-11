a9ba0549c91ba191ec40502ec960fe98d24ed8d0
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-08-11 21:47:14 +0200

    solved lab

--- index.js
+++ index.js
@@ -15,9 +15,67 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+ // Run your code here, after you have insured that the connection was made
+ .then(() => {
+  createSingleRecipeObjectUsingModel()
+  .then(() => createMultipleRecipeObjects())
+  .then(() => updateOneSavedRecipeData())
+  .then(() => deleteOneSaveDRecipetData())
+})
+  .then(() => closeConnect())
   .catch(error => {
     console.error('Error connecting to the database', error);
+  });
+
+const recipeObj = {
+  title: 'menemen',
+  level: 'Easy Peasy',
+  ingredients: [
+    '3 egges',
+    '1 onion',
+    '2 peppers',
+    '3 tomatos',
+    'salt',
+    '1 tablespoons olive oil'
+  ],
+  cuisine: 'Turkish',
+  dishType: 'breakfast',
+  image: 'https://images.media-allrecipes.com/images/75131.jpg',
+  duration: 20,
+  creator: 'Ece',
+};
+
+// CREATE ONE
+const createSingleRecipeObjectUsingModel = () =>
+  Recipe.create(recipeObj)
+    .then((results) => console.log(`Saved new recipe: ${results.title}`))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+
+// CREATE MANY
+const createMultipleRecipeObjects = () =>
+  Recipe.insertMany(data)
+    .then((results) => results.forEach((recipe) => console.log(recipe.title)))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+
+// UPDATE
+const updateOneSavedRecipeData = () =>
+  Recipe.findOneAndUpdate(
+    { title: 'Rigatoni alla Genovese' },
+    { duration: 100 }
+  )
+    .then(() => console.log(`Recipe is updated: duration has been changed`))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+
+// DELETE
+const deleteOneSaveDRecipetData = () =>
+  Recipe.deleteOne({ title: 'Carrot Cake' })
+    .then(() => console.log(`Recipe successfully deleted`))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+
+const closeConnect = () =>
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log(`Mongo connection disconnected`);
+      process.exit(0);
+    });
   });
